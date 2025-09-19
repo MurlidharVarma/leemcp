a74b34bb75834a01eaf676a76979bf295a36dac5
Murlidhar Varma <murlidhar.varma@gmail.com>
2025-09-19 23:50:12 +0530

    Initializer

---
+++ src/main/java/org/aipeel/leemcp/LeemcpApplication.java
@@ -0,0 +1,13 @@
+package org.aipeel.leemcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeemcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeemcpApplication.class, args);
+	}
+
+}
